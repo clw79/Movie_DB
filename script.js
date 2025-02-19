5530f5aebb029354051513835e13ee49bf93a3b6
Chris Williams <cwdevportfolio@gmail.com>
2025-02-19 13:07:50 -0700

    finished app

--- script.js
+++ script.js
@@ -8,28 +8,24 @@ const result = document.getElementById("result");
 let getMovie = () => {
     let movieName = movieNameRef.value;
     let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
-
-
+    //if search bar is empty
     if (movieName.length <= 0) {
         result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
     }
+    //if movie name is entered
     else {
         fetch(url)
             .then((resp) => resp.json())
             .then((data) => {
-                if (data.Respose == "True") {
-
-
-
-
+                if (data.Response == "True") {
                     result.innerHTML = `
                 <div class="info">
                     <img src=${data.Poster} class="poster">
                     <div>
                         <h2>${data.Title}</h2>
                         <div class="rating">
-                            <img src="star-icon.svg">
-                            <h4>${data.imdbRating}</h4>
+                            <span>&#x2B50</span>
+                            <h4>${data.imdbRating} / 10</h4>
                         </div>
                         <div class="details">
                             <span>${data.Rated}</span>
@@ -40,8 +36,7 @@ let getMovie = () => {
                             <div>${data.Genre.split(",").join("</div><div>")}</div>
                         </div>  
                     </div> 
-                </div>         
-            </div> 
+                </div>     
             <h3>Plot:</h3>
             <p>${data.Plot}</p>
             <h3>Cast:</h3>
@@ -57,8 +52,17 @@ let getMovie = () => {
                 result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
             });
 
+        movieNameRef.value = ("");
+
     };
 }
 
 searchBtn.addEventListener("click", getMovie);
 window.addEventListener("load", getMovie);
+
+window.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        getMovie();
+
+    }
+});
