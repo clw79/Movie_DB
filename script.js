e1ed0e0370273546004703f41adf2f3f0e87d447
Chris Williams <cwdevportfolio@gmail.com>
2025-04-01 12:15:47 -0600

    securing api key

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 
+const apiKey = process.env.API_KEY;
 const movieNameRef = document.getElementById("movie-name");
 const searchBtn = document.getElementById("search-btn");
 const result = document.getElementById("result");
@@ -7,7 +9,7 @@ const result = document.getElementById("result");
 //Fetching data from api
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${apiKey}`;
     //if search bar is empty
     if (movieName.length <= 0) {
         result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
